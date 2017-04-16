ad822bb0ba3d84dcc79971b8029bc8366572fe40
David Streid <dcs2153@columbia.edu>
2017-04-16 00:59:40 -0700

    Working, could fix formatting though

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,7 @@ const appRoutes: Routes = [
 	{
 		path: 'greeting',
 		component: GreetingComponent
-	},
-  {
-    path: '',
-    redirectTo: '/greeting',
-    pathMatch: 'full'
-  },
+	}
 ];
 
 
--- src/app/greeting.component.ts
+++ src/app/greeting.component.ts
@@ -1,10 +1,15 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {GREETINGS} from './greetings';
 
 @Component({
 	selector: 'greeting',
-	templateUrl: './greeting.component.html'
+	templateUrl: './greeting.component.html',
+	styleUrls: ['./allGreetings.component.css']
 })
 
-export class GreetingComponent {}
-
-
+export class GreetingComponent implements OnInit {
+	greetings: Object[];
+	ngOnInit(): void {
+		this.greetings = GREETINGS;
+	}
+}
